dd5fe78200f05dd8601ffcb990444cd5104448d8
avisek898 <avisek898@gmail.com>
2022-05-25 02:12:26 -0500

    this is a change

---
+++ 04_functions/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func greeting(name string) string {
+	return "Hello" + name
+}
+func getSum(num1, num2 int) int {
+	return num1 + num2
+}
+
+func main() {
+	fmt.Println(greeting("Abhishek"))
+	fmt.Println(getSum(5, 9))
+}
